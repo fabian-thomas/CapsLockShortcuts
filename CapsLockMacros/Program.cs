96f35b2dc6a28e635b7a12bb2dcc9cef8cefa4c0
Fabian Thomas <fabithomi@web.de>
2020-03-17 16:23:28 +0100

    implemented first basic caps lock macros

--- CapsLockMacros/Program.cs
+++ CapsLockMacros/Program.cs
@@ -4,19 +4,30 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
-namespace DisableCapsLock
+namespace CapsLockMacros
 {
     class Program
     {
 
         #region CAPSLOCK blockieren
 
+        // https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
         private const byte VK_CAPSLOCK = 0x14;
+        private const byte VK_UP = 0x26;
+        private const byte VK_DOWN = 0x28;
+        private const byte VK_LEFT = 0x25;
+        private const byte VK_RIGHT = 0x27;
+        private const byte VK_BACK = 0x08;
+        private const byte VK_DELETE = 0x2E;
+        private const byte VK_HOME = 0x24;
+        private const byte VK_END = 0x23;
+
         private const uint KEYEVENTF_EXTENDEDKEY = 1;
         private const int KEYEVENTF_KEYUP = 0x2;
         private const int KEYEVENTF_KEYDOWN = 0x0;
 
         private const int WM_KEYUP = 0x101;
+        private const int WM_KEYDOWN = 0x0100;
 
         private const int WH_KEYBOARD_LL = 13;
 
@@ -29,29 +40,75 @@ namespace DisableCapsLock
             using (Process curProcess = Process.GetCurrentProcess())
             using (ProcessModule curModule = curProcess.MainModule)
             {
-
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
-
-                    GetModuleHandle(curModule.ModuleName), 0);
-
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
             }
         }
 
         private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
+        private static bool CapsLockPressed = false;
+
+        private static void SimulateKeyPress(IntPtr wParam, byte key)
+        {
+            if (wParam == (IntPtr)WM_KEYDOWN)
+                keybd_event(key, 0x0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYDOWN, 0);
+            else if (wParam == (IntPtr)WM_KEYUP)
+                keybd_event(key, 0x0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
         private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP)
+            if (nCode >= 0)
             {
                 int vkCode = Marshal.ReadInt32(lParam);
                 Keys key = (Keys)vkCode;
-                if (key == Keys.CapsLock && Control.IsKeyLocked(Keys.CapsLock))
+                if (key == Keys.CapsLock)
                 {
-                    Timer.Start();
+                    if (wParam == (IntPtr)WM_KEYUP)
+                    {
+                        CapsLockPressed = false;
+                        if (Control.IsKeyLocked(Keys.CapsLock))
+                            Timer.Start();
+                    }
+                    else CapsLockPressed = true;
+                }
+                else if (CapsLockPressed)
+                {
+                    var filtered_key = false;
+
+                    #region Arrrow Keys
+
+                    if (filtered_key = key == Keys.I)
+                        // bScan = Hardware Scan code; is ignored
+                        SimulateKeyPress(wParam, VK_UP);
+                    else if (filtered_key = key == Keys.K)
+                        SimulateKeyPress(wParam, VK_DOWN);
+                    else if (filtered_key = key == Keys.J)
+                        SimulateKeyPress(wParam, VK_LEFT);
+                    else if (filtered_key = key == Keys.L)
+                        SimulateKeyPress(wParam, VK_RIGHT);
+
+                    #endregion
+
+                    #region Special Keys
+
+                    else if (filtered_key = key == Keys.U)
+                        SimulateKeyPress(wParam, VK_BACK);
+                    else if (filtered_key = key == Keys.O)
+                        SimulateKeyPress(wParam, VK_DELETE);
+                    else if (filtered_key = key == Keys.H)
+                        SimulateKeyPress(wParam, VK_HOME);
+                    else if (filtered_key = key == Keys.Oemtilde)
+                        SimulateKeyPress(wParam, VK_END);
+
+                    #endregion
+
+                    if (filtered_key)
+                        return new IntPtr(1);
                 }
             }
 
-            return CallNextHookEx(_hookID, nCode, wParam, lParam); ;
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
 
         private static void Timer_Tick(object sender, EventArgs e)
@@ -64,7 +121,6 @@ namespace DisableCapsLock
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
--- CapsLockMacros/Properties/AssemblyInfo.cs
+++ CapsLockMacros/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("DisableCapsLockFunction")]
+[assembly: AssemblyTitle("CapsLockMacros")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DisableCapsLockFunction")]
+[assembly: AssemblyProduct("CapsLockMacros")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
